165d2fa8d7bdc8d59a26fe5bdef8386f24875c9f
Yahor Ruban <rubanyahor@gmail.com>
2021-10-28 10:20:37 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Cone
+    {
+        private double Radius { get; set; }
+        private double High { get; set; }
+
+        public Cone(double r, double h)
+        {
+            Radius = r;
+            High = h;
+        }
+
+        public double SurfaceArea()
+        {
+            return (Math.PI * (Math.Pow(Radius, 2)));
+        }
+
+        public double FullArea()
+        {
+            return (Math.PI * Radius * (Math.Sqrt(Math.Pow(Radius, 2) + Math.Pow(High, 2)) + Radius));
+        }
+
+    }
+}
---
+++ lesson5Part3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace lesson5Part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cone cone = new Cone(3.0, 1.0);
+            double x = cone.SurfaceArea();
+            Console.WriteLine(x);
+            double y = cone.FullArea();
+            Console.WriteLine(y);
+        }
+    }
+}
