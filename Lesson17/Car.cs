ec3d42644da373b0eedf28cc1ce828e88fea96eb
Yahor Ruban <rubanyahor@gmail.com>
2021-12-22 16:10:38 +0300

    Lesson17

---
+++ Lesson17/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17
+{
+    public class Car
+    {
+        private int Age { get; set; }
+
+        public Car(int age)
+        {
+            Age = age;
+        }
+
+        public void ChangeAge(int age)
+        {
+            Age = age;
+        }
+    }
+}
---
+++ Lesson17/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+
+            var carsList = new List<Car>();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                carsList.Add(new Car(i));
+            }
+
+            stopWatch.Start();
+            foreach (var car in carsList)
+            {
+                car.ChangeAge((343 * 34 * DateTime.Now.Millisecond * 77) / DateTime.Now.Minute);
+            }
+            stopWatch.Stop();
+            TimeSpan ts2 = stopWatch.Elapsed; 
+            Console.WriteLine(ts2);
+
+            stopWatch.Start();
+            for (int i = 0; i < carsList.Count; i++)
+            {
+                carsList[i].ChangeAge((343 * 34 * DateTime.Now.Millisecond * 77) / DateTime.Now.Minute);
+            }
+            stopWatch.Stop();
+            TimeSpan ts3 = stopWatch.Elapsed;
+            Console.WriteLine(ts3);
+
+            stopWatch.Start();
+            Parallel.ForEach(carsList, car => car.ChangeAge((343 * 34 * DateTime.Now.Millisecond * 77) / DateTime.Now.Minute));
+            stopWatch.Stop();
+            TimeSpan ts4 = stopWatch.Elapsed;
+            Console.WriteLine(ts4);
+
+            Action<int> ChangeCarAge = (int i) =>
+            {
+                carsList[i].ChangeAge((343 * 34 * DateTime.Now.Millisecond * 77) / DateTime.Now.Minute);
+            };
+
+            stopWatch.Start();
+            Parallel.For(0, carsList.Count, ChangeCarAge);
+            stopWatch.Stop();
+            TimeSpan ts5 = stopWatch.Elapsed;
+            Console.WriteLine(ts5);
+        }
+    }
+}
